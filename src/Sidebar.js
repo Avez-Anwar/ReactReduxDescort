5e2222a41313d88da5b466d0dc5a03bd74bc44ba
Avez-Anwar <34218292+Avez-Anwar@users.noreply.github.com>
2020-10-10 21:11:54 +0530

    second commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,20 +2,49 @@ import React from "react";
 import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add"
+import SidebarChannel from "./SidebarChannel";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlindeIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sidebar__top">The drop down
+      <div className="sidebar__top">
+      <h3>The drop down</h3>
       <ExpandMoreIcon /> 
       </div>
+
       <div className="sidebar__channels">
-      <div className="sidebar__channelsHeader">
-      <div className="sidebar__header">
-      <ExpandMoreIcon/>
-      <h4>Text Channel</h4>
+       <div className="sidebar__channelsHeader">
+        <div className="sidebar__header">
+         <ExpandMoreIcon/>
+         <h4>Text Channel</h4>
       </div>
+
       <AddIcon className="sidebar__addChannel" />
+      </div>
+
+      
+       <div className="sidebar__channelsList">
+            <SidebarChannel/>
+            <SidebarChannel/>
+            <SidebarChannel/>
+            <SidebarChannel/>
+       </div>
+      </div>
+
+      <div className="sidebar__voice">
+      <SignalCellularAltIcon
+      className="sidebar__voiceIcon"
+      fontSize="large"/>
+      <div className="sidebar__voiceInfo">
+      <h3>Voice Connected</h3>
+      <p>stream</p>
+      </div>
+      <div className="sidebar__voiceIcons">
+      <InfoOutlindeIcon/>
+      <CallIcon/>
       </div>
       </div>
     </div>
